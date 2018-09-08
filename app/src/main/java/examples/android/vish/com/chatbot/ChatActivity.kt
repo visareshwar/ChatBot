17a0e274c9bb6abe7807ac9356a8f8def0ecd716
Vishu <vishwanath@nestaway.com>
2018-09-08 20:46:13 +0530

    creating project

---
+++ app/src/main/java/examples/android/vish/com/chatbot/ChatActivity.kt
@@ -0,0 +1,12 @@
+package examples.android.vish.com.chatbot
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
