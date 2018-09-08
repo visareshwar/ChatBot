cacc7fb50026975824e9c7378e30b321d77189c5
Vishu <vishwanath@nestaway.com>
2018-09-08 22:53:04 +0530

    9. Network call for getting bot response

--- app/src/main/java/examples/android/vish/com/chatbot/db/repo/ConversationNetworkRepo.kt
+++ app/src/main/java/examples/android/vish/com/chatbot/db/repo/ConversationNetworkRepo.kt
@@ -1,11 +1,49 @@
 package examples.android.vish.com.chatbot.db.repo
 
 import android.arch.lifecycle.MutableLiveData
+import android.text.TextUtils
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.google.gson.Gson
 import examples.android.vish.com.chatbot.db.model.Conversation
+import org.json.JSONObject
 
-class ConversationNetworkRepo {
+class ConversationNetworkRepo private constructor() {
     val currentPrListLiveData: MutableLiveData<Conversation> = MutableLiveData()
 
+    companion object {
+        @Volatile
+        private var INSTANCE: ConversationNetworkRepo? = null
 
+        fun getInstance(): ConversationNetworkRepo =
+                INSTANCE ?: synchronized(this) {
+                    INSTANCE
+                            ?: ConversationNetworkRepo().also { ConversationNetworkRepo.INSTANCE = it }
+                }
+
+        private val URL = "https://www.personalityforge.com/api/chat/?apiKey=6nt5d1nJHkqbkphe&chatBotID=63906&externalID=chirag1&message="
+
+    }
+
+    fun getBotResponse(message: String) {
+
+        val request = JsonObjectRequest(URL + message, null,
+                Response.Listener<JSONObject> { response ->
+                    Log.d("ConversationNetworkRepo", response.toString())
+                    val conversation = response.optJSONObject("message")
+                    if (conversation != null && TextUtils.isEmpty(conversation.toString())) {
+                        val gson = Gson()
+                        val conversationEntity = gson.fromJson(conversation.toString(), Conversation::class.java)
+                        currentPrListLiveData.postValue(conversationEntity)
+                    }
+
+                },
+
+
+                Response.ErrorListener {
+
+                })
+    }
 
 }
